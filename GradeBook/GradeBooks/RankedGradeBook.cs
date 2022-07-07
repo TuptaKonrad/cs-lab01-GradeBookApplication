4e8633ca0efd247a538144e6a3d3415246ccfa68
konrad.tupta <konrad.tupta@SN5>
2022-07-07 15:11:45 +0200

    Override RankedGradeBook's CalculateStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -31,5 +31,14 @@ namespace GradeBook.GradeBooks
                 return 'F';
 
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else base.CalculateStatistics();
+        }
     }
 }
